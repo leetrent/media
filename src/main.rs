361dae6692fc68cbc6aaee1dad3d411bf28aec77
LeeTrent <lee.trent.1@gmail.com>
2024-08-19 07:25:09 -0500

    Declaring and printing enum values

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,37 @@
 fn main() {
+    /////////////////////////////////////////
+    // AUDIOBOOK
+    /////////////////////////////////////////
     let audiobook = Media::Audiobook {
         title: String::from("Audiobook #1")
     };
 
     println!("\n--------------------------");
     print_media(audiobook);
+    println!("--------------------------\n");
+
+    /////////////////////////////////////////
+    // MOVIE
+    /////////////////////////////////////////
+    let good_movie = Media::Movie {
+        title: String::from("Good Movide"),
+        director: String::from("Good Director)")
+    };
+
+    println!("\n--------------------------");
+    print_media(good_movie);
+    println!("--------------------------\n");
+
+    /////////////////////////////////////////
+    // BOOK
+    /////////////////////////////////////////
+    let bad_book = Media::Book {
+        title: String::from("Bad Book"),
+        author: String::from("Bad Author")
+    };
+
+    println!("\n--------------------------");
+    print_media(bad_book);
     println!("--------------------------\n");
 }
 
