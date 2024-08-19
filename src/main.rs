13b9212841556df590156b9b31aeaa28a06339e4
LeeTrent <lee.trent.1@gmail.com>
2024-08-19 08:18:23 -0500

    Added description implementation to enum

--- src/main.rs
+++ src/main.rs
@@ -6,21 +6,21 @@ fn main() {
         title: String::from("Audiobook #1")
     };
 
-    println!("\n--------------------------");
-    print_media(audiobook);
-    println!("--------------------------\n");
+    // println!("\n--------------------------");
+    // print_media(audiobook);
+    // println!("--------------------------\n");
 
     /////////////////////////////////////////
     // MOVIE
     /////////////////////////////////////////
     let good_movie = Media::Movie {
-        title: String::from("Good Movide"),
-        director: String::from("Good Director)")
+        title: String::from("Good Movie"),
+        director: String::from("Good Director")
     };
 
-    println!("\n--------------------------");
-    print_media(good_movie);
-    println!("--------------------------\n");
+    // println!("\n--------------------------");
+    // print_media(good_movie);
+    // println!("--------------------------\n");
 
     /////////////////////////////////////////
     // BOOK
@@ -30,9 +30,18 @@ fn main() {
         author: String::from("Bad Author")
     };
 
+    // println!("\n--------------------------");
+    // print_media(bad_book);
+    // println!("--------------------------\n");
+
+    /////////////////////////////////////////
+    // DESCRIPTON
+    /////////////////////////////////////////
     println!("\n--------------------------");
-    print_media(bad_book);
-    println!("--------------------------\n");
+    println!("{}", audiobook.description());
+    println!("{}", good_movie.description());
+    println!("{}", bad_book.description());
+    println!("\n--------------------------");
 }
 
 #[derive(Debug)]
@@ -42,6 +51,20 @@ enum Media {
     Audiobook {title: String }
 }
 
-fn print_media(media: Media) {
-    println!("{:#?}", media);
+impl Media {
+    fn description(&self) -> String{
+        if let Media::Book {title, author} = self {
+            format!("Book: {} {}", title, author)
+        } else if let Media::Movie {title, director} = self {
+            format!("Movie: {} {}", title, director)
+        } else if let Media::Audiobook { title } = self {
+            format!("Audiobook: {}", title)
+        } else {
+            String::from("Media Description")
+        }
+    }
 }
+
+// fn print_media(media: Media) {
+//     println!("{:#?}", media);
+// }
